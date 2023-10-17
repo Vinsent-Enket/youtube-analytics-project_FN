1a755ca55bd90bc24c426b541c62768836628662
filippov nikita <filippovnr73@gmail.com>
2023-10-17 14:37:20 +0300

    howo 5 complited

--- src/video.py
+++ src/video.py
@@ -1,17 +1,13 @@
-import os
-from googleapiclient.discovery import build
-
-api_key: str = os.getenv('YT_API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
+from src.apimixin import APIMixin
 
 
-class Video:
+class Video(APIMixin):
 
     def __init__(self, id_video: str) -> None:
-
+        super().__init__()
         """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
 
-        self.video = youtube.videos().list(id=id_video, part='snippet,statistics').execute()
+        self.video = self.youtube.videos().list(id=id_video, part='snippet,statistics').execute()
         self.id = self.video['items'][0]['id']
         self.title = self.video['items'][0]['snippet']['title']
         self.url = self.video['items'][0]['snippet']['thumbnails']['default']['url']
@@ -33,9 +29,12 @@ class PLVideo(Video):
     """
     Откровенно говоря не понял, а зачем id видео и id playlist если нам нужна инфа именно по видео?
     Да и судя по метод принимает лишь 1 аргумент, один id
-    И да, хоть и стоит метод videos он исправна дает всю нужную инфу
+    И да, хоть и стоит метод videos он исправно дает всю нужную инфу
     Но код работает
     """
+
     def __init__(self, id_video: str, id_playlist):
         super().__init__(id_video)
-        self.play_list = youtube.videos().list(id=id_playlist, part='snippet,statistics').execute()
+        self.play_list = self.youtube.playlists().list(id=id_playlist, part='contentDetails,snippet',
+                                                  maxResults=50, ).execute()
+        self.play_list_id = self.play_list['items'][0]['id']
